77c1db1a0bcddf9ec54700a94885819ffa9dae04
mohammadmahdijerban <mohammadmahdijerban@gmail.com>
2025-10-18 13:37:41 +0330

    erorr

--- js/app.js
+++ js/app.js
@@ -1,16 +1,51 @@
 const userForm = document.getElementsByTagName("form")[0];
 
+const firstNameInput = document.getElementsByTagName("input")[0];
+const lastNameInput = document.getElementsByTagName("input")[1];
+const ageInput = document.getElementsByTagName("input")[2];
+const fatherNameInput = document.getElementsByTagName("input")[3];
+const nationalCodeInput = document.getElementsByTagName("input")[4];
+
 userForm.addEventListener("submit", (e) => {
-    
-  console.log(e.target.firstName.value);
+  const user = {
+    firstName: e.target.firstName.value,
+    lastName: e.target.lastName.value,
+    age: e.target.age.value,
+    fatherName: e.target.fatherName.value,
+    nationalCode: e.target.nationalCode.value,
+  };
 
-  console.log(e.target.lastName.value);
-
-  console.log(e.target.age.value);
-
-  console.log(e.target.fatherName.value);
-
-  console.log(e.target.nationalCode.value);
+  console.log(user);
 
   e.preventDefault();
+});
+
+firstNameInput.addEventListener("blur", (e) => {
+  if (firstNameInput.value.length < 3) {
+    alert("your name isn`t right");
+  }
+});
+
+lastNameInput.addEventListener("blur", (e) => {
+  if (lastNameInput.value.length < 3) {
+    alert("your last name isn`t right");
+  }
+});
+
+ageInput.addEventListener("blur", (e) => {
+  if (ageInput.value < 2) {
+    alert("your age isn`t right");
+  }
+});
+
+fatherNameInput.addEventListener("blur", (e) => {
+  if (fatherNameInput.value.length < 3) {
+    alert("your father name isn`t right");
+  }
+});
+
+nationalCodeInput.addEventListener("blur", (e) => {
+  if (nationalCodeInput.value.length < 10 || nationalCodeInput.value.length > 10) {
+    alert("your national code isn`t right");
+  }
 });
